95a3a569bce2bb60eaf4d3b17920887a25e68185
PoorvaBhalerao <151721149+PoorvaBhalerao@users.noreply.github.com>
2025-11-06 12:02:22 +0530

    Add files via upload

---
+++ program19.java
@@ -0,0 +1,17 @@
+//Display "Jay Ganesh" four times on screen.
+
+class program19
+{
+    public void Display()
+    {
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+    }
+
+    public static void main(String A[])
+    {        
+        Display();      // Error
+    }
+}
---
+++ program20.java
@@ -0,0 +1,19 @@
+//Display "Jay Ganesh" four times on screen.
+
+// Better approach
+
+class program20
+{
+    public static void Display()
+    {
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+    }
+
+    public static void main(String A[])
+    {        
+        Display();      
+    }
+}
